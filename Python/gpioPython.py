aadb489e4ca5e18e7a734d87369ea8743b63b37c
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-19 18:43:04 +0000

    Adding gpio Python assignment code

--- Python/gpioPython.py
+++ Python/gpioPython.py
@@ -2,18 +2,21 @@ import RPi.GPIO as GPIO
 import time
 
 green = 12
+red = 18
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(green, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
 
 while True:
 	GPIO.output(green, GPIO.HIGH)
-	print("LED on!")
-	time.sleep(.2)
+	#print("LED on!")
+	time.sleep(.5)
 	GPIO.output(green, GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(red, GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(red, GPIO.LOW)
 	exit()
-	#else: 
-		#print("Program Complete!")
-		#GPIO.cleanup()
-		#exit()
+
