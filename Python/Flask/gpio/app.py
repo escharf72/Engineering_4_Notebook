0aac6d89a9bca7540a61291f15ba71b19e9e7923
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-29 20:41:27 +0000

    adding Flask code!

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -4,18 +4,23 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-     if request.method == "POST":
+     if request.method == "POST" and request.form.get("submitBtn"):
           GPIO.output(17,GPIO.HIGH)
-          msg = request.form.get("submitBtn")
-     else:
+          GPIO.output(18,GPIO.LOW)
+          msg = "welcome!"
+     if request.method == "POST" and request.form.get("submitBtn2"):
+          GPIO.output(18,GPIO.HIGH)
           GPIO.output(17,GPIO.LOW)
-          msg = "No click yet."
+          msg = "Welcome!"
+     else:
+          msg = "Welcome!"
      return render_template("index.html", msg=msg)
 
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=80)
+     app.run(host="0.0.0.0", port=8080)
