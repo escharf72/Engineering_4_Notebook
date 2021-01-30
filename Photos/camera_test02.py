b2ee8c4ea3f79b34fb825d5e68acdd805875ba22
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-30 13:48:43 +0000

    almond butter test

--- Photos/camera_test02.py
+++ Photos/camera_test02.py
@@ -1,6 +1,5 @@
 import time
 import picamera
-#class picamera.camera.PiCamera(camera_num=0, stereo_mode='none', stereo_decimate=False, resolution=None, framerate=None, sensor_mode=0, led_pin=None) 
 val = 0
 with picamera.PiCamera() as camera:
     effects = ['watercolor', 'cartoon', 'oilpaint', 'film', 'pastel']
