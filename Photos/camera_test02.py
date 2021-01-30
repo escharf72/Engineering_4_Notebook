ac3a4fae606157f801785efa0523619efcc318d4
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-30 15:50:53 +0000

    I think I figured out a good file naming system!

--- Photos/camera_test02.py
+++ Photos/camera_test02.py
@@ -1,7 +1,9 @@
 import time
 import picamera
+from datetime import datetime
 #capture_continuous(output, format=None, use_video_port=False, resize=None, splitter_port=0, burst=False, **options)
 val = 0
+filename = "{0:%c}".format(datetime.now())
 with picamera.PiCamera() as camera:
     effects = ['watercolor', 'cartoon', 'oilpaint', 'film', 'pastel']
     while val <= 4:
@@ -12,8 +14,7 @@ with picamera.PiCamera() as camera:
         camera.image_effect = x
         camera.annotate_text = x 
         time.sleep(2)
-        #camera.capture('Image'+str(val)+'.jpg')
-        camera.capture_continuous('image{counter:02d}.jpg')
+        camera.capture(str(val)+filename+".jpg")
         print("Done")
         val = val+1
     else:
