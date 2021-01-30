b3257a730f746b23c7d0e040bd888ad86ab85022
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-30 13:25:25 +0000

    adding photos to look at them

--- Photos/camera_test02.py
+++ Photos/camera_test02.py
@@ -1,21 +1,17 @@
 import time
 import picamera
-class picamera.camera.PiCamera(camera_num=0, stereo_mode='none', stereo_decimate=False, resolution=None, framerate=None, sensor_mode=0, led_pin=None) 
-count = 0
+#class picamera.camera.PiCamera(camera_num=0, stereo_mode='none', stereo_decimate=False, resolution=None, framerate=None, sensor_mode=0, led_pin=None) 
+val = 0
 with picamera.PiCamera() as camera:
-    print("Getting ready.... say cheese!")
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    for effect in camera.IMAGE_EFFECTS:
-        camera.image_effect = effect
-        camera.annotate_text = "Effect: %s" % effect
-    time.sleep(2)
-    camera.capture('YourPhoto.jpg')
-    print("Done")
-
-camera = PiCamera()
-try:
-    # do something with the camera
-    pass
-finally:
-    camera.close()
+    effects = ['watercolor', 'cartoon', 'oilpaint', 'film', 'pastel']
+    while val <= 4:
+        print("Getting ready.... say cheese!")
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        camera.image_effect = effects[val] 
+        time.sleep(2)
+        camera.capture('YourPhoto.jpg')
+        print("Done")
+        val = val+1
+    else:
+        camera.close() 
