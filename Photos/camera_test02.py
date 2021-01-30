ad21cdd8184b0f068b19042e832e309be4af3a0b
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-30 01:01:10 +0000

    Hopefully adding photos

--- Photos/camera_test02.py
+++ Photos/camera_test02.py
@@ -1,21 +1,32 @@
 import time
 import picamera
 count = 0
-0 = 'oilpaint'
-1 = 'pastel'
-2 = 'cartoon'
-3 = 'watercolor'
-4 = 'solarize'
 with picamera.PiCamera() as camera:
-    while count <= 4:
-        print("Getting ready.... say cheese!")
-        camera.resolution = (1024, 768)
-        camera.image_effect(count)
-        camera.start_preview()
+    #while count <= 4:
+    print("Getting ready.... say cheese!")
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    for effect in camera.IMAGE_EFFECTS:
+        camera.image_effect = effect
+        camera.annotate_text = "Effect: %s" % effect
+        #time.sleep(5)
+    #camera.stop_preview()
+       # while count == 0:
+        #        camera.image_effect = 'oilpaint'
+        #while count == 1:
+         #       camera.image_effect = 'pastel'
+        #while count == 2:
+        #        camera.image_effect = 'cartoon'
+        #while count == 3:
+        #        camera.image_effect = 'watercolor'
+        #while count == 4: 
+         #       camera.image_effect = 'solarize'
+       # camera.start_preview()
         # Camera warm-up time
         time.sleep(2)
         camera.capture('YourPhoto.jpg')
-    else:
-        print("Done!")
-        exit()
+        #count = count + 1
+#else:
+    #print("Done!")
+    #exit()
 
