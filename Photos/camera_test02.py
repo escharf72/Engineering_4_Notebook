c95084468f6b6d6da1daec83755f133b25e89165
escharf72 <escharf72@charlottesvilleschools.org>
2021-01-30 02:10:21 +0000

    Adding code for the night

--- Photos/camera_test02.py
+++ Photos/camera_test02.py
@@ -1,32 +1,21 @@
 import time
 import picamera
+class picamera.camera.PiCamera(camera_num=0, stereo_mode='none', stereo_decimate=False, resolution=None, framerate=None, sensor_mode=0, led_pin=None) 
 count = 0
 with picamera.PiCamera() as camera:
-    #while count <= 4:
     print("Getting ready.... say cheese!")
     camera.resolution = (1024, 768)
     camera.start_preview()
     for effect in camera.IMAGE_EFFECTS:
         camera.image_effect = effect
         camera.annotate_text = "Effect: %s" % effect
-        #time.sleep(5)
-    #camera.stop_preview()
-       # while count == 0:
-        #        camera.image_effect = 'oilpaint'
-        #while count == 1:
-         #       camera.image_effect = 'pastel'
-        #while count == 2:
-        #        camera.image_effect = 'cartoon'
-        #while count == 3:
-        #        camera.image_effect = 'watercolor'
-        #while count == 4: 
-         #       camera.image_effect = 'solarize'
-       # camera.start_preview()
-        # Camera warm-up time
-        time.sleep(2)
-        camera.capture('YourPhoto.jpg')
-        #count = count + 1
-#else:
-    #print("Done!")
-    #exit()
+    time.sleep(2)
+    camera.capture('YourPhoto.jpg')
+    print("Done")
 
+camera = PiCamera()
+try:
+    # do something with the camera
+    pass
+finally:
+    camera.close()
